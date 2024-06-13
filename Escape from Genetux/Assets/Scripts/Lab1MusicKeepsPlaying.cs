70662b7aa071304816730a8b1ba73d63efa4d795
SophieKhuu <166718926+SophieKhuu@users.noreply.github.com>
2024-06-13 11:32:47 +0200

    Pelotes visibles & correction bug musique

--- Escape from Genetux/Assets/Scripts/Lab1MusicKeepsPlaying.cs
+++ Escape from Genetux/Assets/Scripts/Lab1MusicKeepsPlaying.cs
@@ -6,6 +6,7 @@ public class Lab1MusicKeepsPlaying : MonoBehaviour
 {
     private void Awake()
     {
+        Destroy(GameObject.Find("Lab2Music"));
         GameObject[] musicObj = GameObject.FindGameObjectsWithTag("MusicLab1");
         if(musicObj.Length > 1){
             Destroy(this.gameObject);
